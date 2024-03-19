a4d03884921d62ce6d9f4f35f8ff4acb0c4642cd
Max Emerson <maxemerson2013@gmail.com>
2024-03-19 13:32:42 +0000

    finished workshop and began to add game

--- typescriptworkshop/src/app/game.js
+++ typescriptworkshop/src/app/game.js
@@ -1,10 +1,30 @@
-class Player {
-  constructor(name, att, def, str, hp) {
+class Weapon {
+  constructor(name, att, str, def, dmg) {
     this.name = name;
     this.att = att;
-    this.def = def;
     this.str = str;
+    this.def = def;
+    this.dmg = dmg;
+  }
+  attack() {
+    console.log(`${this.name} dealt ${this.dmg} damage`);
+  }
+}
+
+class Axe extends Weapon {
+  constructor(name, att, str, def, dmg) {
+    super(name, att, str, def, dmg);
+  }
+  slash() {
+    console.log(`${this.name} used slash attack and dealt ${this.dmg}`);
+  }
+}
+
+class Player {
+  constructor(name, hp, weapon) {
+    this.name = name;
     this.hp = hp;
+    this.weapon = weapon;
   }
   attack(defender) {
     if (this.def >= 5) {
@@ -18,27 +38,18 @@ class Player {
   }
 }
 
-class Weapon {
-  constructor(name, att, str, def, dmg) {
-    this.name = name;
-    this.att = att;
-    this.str = str;
-    this.def = def;
-    this.dmg = dmg;
-  }
-  attack() {
-    console.log(`${this.name} dealt ${this.dmg}`);
-  }
-}
-
-class Axe extends Weapon {
-  constructor(name, att, str, def, dmg) {
-    super(name, att, str, def, dmg);
-  }
-  slash() {}
-}
-
+const Staff = new Weapon("Staff", 3, 2, 0, 5);
 const Max = new Player("Max", 8, 3, 3, 15);
 const Jerry = new Player("Jerry", 2, 10, 25, 45);
+const newAxe = new Axe("Axe", 20, 10, -2, 6);
 
 Max.attack(Jerry);
+newAxe.slash();
+
+Staff.attack();
+
+//step by step for creating this game:
+//1 - create weapon class with general stats and maybe attack styles
+//2 - create specific weapons with stats using const Axe = new Weapon("Axe", relevant stats and attack style).
+//3 - create player with name, hp and weapon
+//4 - call the relevant player and attack method and adjust stats as necessary
